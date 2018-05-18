b52b3d5e750ea8a7a7c40db7d4d1f116ae9dfe52
Edsel Caprice <caprice@mail.usf.edu>
2018-05-18 02:38:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   bobMarly: "is this love"
 };
 
-function updatePlaylist(list,name,song) {
-  Object.assign({},list,{[name]:song});
+function updatePlaylist(playlist,artistName,songTitle) {
+  Object.assign({},playlist,{[artistName]:songTitle});
 }
